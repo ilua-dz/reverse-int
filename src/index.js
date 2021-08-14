84184ce812317ac71c665d4b0832168f35fbb7e7
Ilya Dzyuin <86766397+ilua-dz@users.noreply.github.com>
2021-08-14 10:20:35 +0400

    feat: add task solution to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  	if (n >= 0) {
+		return Number(String(n).split('').reverse().join(''));
+  } else {
+		return 'Please enter a positive number';
+  }
 }
