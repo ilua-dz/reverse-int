96ca55f45957b3c8dc5fcd3e97dabdf5125b744d
Ilya Dzyuin <86766397+ilua-dz@users.noreply.github.com>
2021-08-14 10:26:55 +0400

    fix:  error correction

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 module.exports = function reverse (n) {
-  	if (n >= 0) {
-		return Number(String(n).split('').reverse().join(''));
-  } else {
-		return 'Please enter a positive number';
+        let nAbs = Math.abs(n);
+		return Number(String(nAbs).split('').reverse().join(''));
   }
-}
